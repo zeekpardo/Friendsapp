d3b2fbf0292d7aea6afa38b6ea8ab4d8c5b61b7a
Zeek <zeekpardo@gmail.com>
2022-08-06 23:39:22 -0700

    end of tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  devise_for :users
+  resources :friends
+  #get 'home/index'
+  get 'home/about'
+  #root "home#index"
+  root "friends#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
